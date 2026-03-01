527cd17bb4c72a879bbc4ee24bb02c90ce659e26
Matthias Wolff <m_wolf37@uni-muenster.de>
2026-03-01 03:18:42 +0100

    Fix missing parenthesis in pyright function completion

--- .config/nvim/lua/matthias/plugins/autopairs.lua
+++ .config/nvim/lua/matthias/plugins/autopairs.lua
@@ -1,7 +1,20 @@
 return {
     'windwp/nvim-autopairs',
     event = "InsertEnter",
-    config = true
-    -- use opts = {} for passing setup options
-    -- this is equivalent to setup({}) function
+    config = function()
+        local npairs = require("nvim-autopairs")
+        npairs.setup({
+            check_ts = true,  -- optional, safer brackets with treesitter
+        })
+
+        -- Hook nvim-cmp so function completions get ()
+        local cmp_status_ok, cmp = pcall(require, "cmp")
+        if cmp_status_ok then
+            local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+            cmp.event:on(
+                "confirm_done",
+                cmp_autopairs.on_confirm_done()
+            )
+        end
+    end
 }
