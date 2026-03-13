b9a1fefaef17238c8a61b1edf243b1b84f97a448
Matthias Wolff <m_wolf37@uni-muenster.de>
2026-03-13 19:20:09 +0100

    change: keymap for fugitive git

--- .config/nvim/lua/matthias/plugins/fugitive.lua
+++ .config/nvim/lua/matthias/plugins/fugitive.lua
@@ -1,10 +1,10 @@
 -- Vim-fugitive
 return {
-    "tpope/vim-fugitive",
-    cmd = { "Git", "G", "Gdiffsplit", "Gvdiffsplit" },
-    init = function()
-        vim.keymap.set("n", "<leader>gg", "<cmd>Git<CR>", { desc = "Git Status" })
-        vim.keymap.set("n", "<leader>gl", "<cmd>Git log -- %<CR>", { desc = "Git Log (file)" })
-        vim.keymap.set("n", "<leader>gL", "<cmd>Git log<CR>", { desc = "Git Log (full)" })
-    end,
+	"tpope/vim-fugitive",
+	cmd = { "Git", "G", "Gdiffsplit", "Gvdiffsplit" },
+	init = function()
+		vim.keymap.set("n", "<leader>g", "<cmd>Git<CR>", { desc = "Git Status" })
+		vim.keymap.set("n", "<leader>gl", "<cmd>Git log -- %<CR>", { desc = "Git Log (file)" })
+		vim.keymap.set("n", "<leader>gL", "<cmd>Git log<CR>", { desc = "Git Log (full)" })
+	end,
 }
