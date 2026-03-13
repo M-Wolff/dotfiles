287f11ecfe69b118cd10bdd96d48445de61ace69
Matthias Wolff <m_wolf37@uni-muenster.de>
2026-03-13 19:33:46 +0100

    revert back to old keymap for fugitive

--- .config/nvim/lua/matthias/plugins/fugitive.lua
+++ .config/nvim/lua/matthias/plugins/fugitive.lua
@@ -3,7 +3,7 @@ return {
 	"tpope/vim-fugitive",
 	cmd = { "Git", "G", "Gdiffsplit", "Gvdiffsplit" },
 	init = function()
-		vim.keymap.set("n", "<leader>g", "<cmd>Git<CR>", { desc = "Git Status" })
+		vim.keymap.set("n", "<leader>gg", "<cmd>Git<CR>", { desc = "Git Status" })
 		vim.keymap.set("n", "<leader>gl", "<cmd>Git log -- %<CR>", { desc = "Git Log (file)" })
 		vim.keymap.set("n", "<leader>gL", "<cmd>Git log<CR>", { desc = "Git Log (full)" })
 	end,
