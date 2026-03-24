4b8c5e144777ba057aec13da474c298d661144d0
Matthias Wolff <m_wolf37@uni-muenster.de>
2026-03-24 19:38:18 +0100

    add: command to push in git fugitive

--- .config/nvim/lua/matthias/plugins/fugitive.lua
+++ .config/nvim/lua/matthias/plugins/fugitive.lua
@@ -6,5 +6,6 @@ return {
 		vim.keymap.set("n", "<leader>gg", "<cmd>Git<CR>", { desc = "Git Status" })
 		vim.keymap.set("n", "<leader>gl", "<cmd>Git log -- %<CR>", { desc = "Git Log (file)" })
 		vim.keymap.set("n", "<leader>gL", "<cmd>Git log<CR>", { desc = "Git Log (full)" })
+		vim.keymap.set("n", "<leader>gp", "<cmd>Git push<CR>", { desc = "Git push to origin" })
 	end,
 }
