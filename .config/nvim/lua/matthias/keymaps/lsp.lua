a76b47babbcfdf33f1b1c95cf840286730c6d5c1
Matthias Wolff <m_wolf37@uni-muenster.de>
2026-09-11 13:39:01 +0200

    change: keymap harpoon & reformat

--- .config/nvim/lua/matthias/keymaps/lsp.lua
+++ .config/nvim/lua/matthias/keymaps/lsp.lua
@@ -1,9 +1,11 @@
 vim.keymap.set("n", "gd", vim.lsp.buf.definition, { silent = true })
 -- Rename symbol under cursor
 vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, { silent = true })
-vim.keymap.set("n", "<leader>R", function() require('telescope.builtin').lsp_references({previewer=false,}) end, { silent = true })
+vim.keymap.set("n", "<leader>R", function()
+	require("telescope.builtin").lsp_references({ previewer = false })
+end, { silent = true })
 -- Auto show errors on hover (250ms)
-vim.o.updatetime = 250  -- milliseconds until CursorHold triggers
+vim.o.updatetime = 250 -- milliseconds until CursorHold triggers
 -- Define the highlights
 vim.cmd([[
   highlight DiagnosticFloatingError guifg=#FFFFFF guibg=#5C1E1E
@@ -12,12 +14,11 @@ vim.cmd([[
   highlight DiagnosticFloatingHint  guifg=#FFFFFF guibg=#1E5C3E
 ]])
 vim.api.nvim_create_autocmd("CursorHold", {
-  pattern = "*",
-  callback = function()
-    vim.diagnostic.open_float(nil, {
-        focus = false,
-        border = "rounded",
-
-    })
-  end,
+	pattern = "*",
+	callback = function()
+		vim.diagnostic.open_float(nil, {
+			focus = false,
+			border = "rounded",
+		})
+	end,
 })
